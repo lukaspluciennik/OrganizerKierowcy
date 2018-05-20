732ee467150e6d93e0919498c0351ae96d72006d
lukasz <lukaspluciennik@gmail.com>
2018-05-20 23:51:39 +0200

    Update

--- app/src/main/java/ukaszpuciennik/organiezerkierowcy/Oil_layout.java
+++ app/src/main/java/ukaszpuciennik/organiezerkierowcy/Oil_layout.java
@@ -5,53 +5,34 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Display;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ListAdapter;
-import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.Toolbar;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 
 public class Oil_layout extends AppCompatActivity {
     static final int DIALOG_ID = 0;
     ArrayList<String> historias = new ArrayList<String>();
-    ArrayList<String> historias_wymiany = new ArrayList<String>();
+    ArrayList<String> historiasWymiany = new ArrayList<String>();
     TextView rodzaj;
-    TextView data_wymiany;
-    TextView przebieg_wymiany;
-    TextView data_sprawdzenia;
-    TextView przebieg_sprawdzenia;
+    TextView dataWymiany;
+    TextView przebiegWymiany;
+    TextView dataSprawdzenia;
+    TextView przebiegSprawdzenia;
     TextView dolewka;
     OilClass oil = new OilClass();
     int year_x, month_x, day_x;
@@ -64,8 +45,8 @@ public class Oil_layout extends AppCompatActivity {
             month_x = monthOfYear + 1;
             day_x = dayOfMonth;
             String dateString = day_x + "-" + month_x + "-" + year_x;
-            TextView data_dzis = (TextView) findViewById(R.id.data_dzisiejsza_s);
-            TextView data_dzis2 = (TextView) findViewById(R.id.data_dzisiejsza_w);
+            TextView data_dzis = findViewById(R.id.data_dzisiejsza_s);
+            TextView data_dzis2 = findViewById(R.id.data_dzisiejsza_w);
             data_dzis.setText(dateString);
             data_dzis2.setText(dateString);
             oil.setData_sprawdzenia(dateString);
@@ -91,12 +72,12 @@ public class Oil_layout extends AppCompatActivity {
 
         //historias = getSavedArrayList();
 
-        data_sprawdzenia = (TextView) findViewById(R.id.data_sprawdzenia);
-        przebieg_sprawdzenia = (TextView) findViewById(R.id.przebieg_sprawdzenia);
-        data_wymiany = (TextView) findViewById(R.id.data_wymiany);
-        przebieg_wymiany = (TextView) findViewById(R.id.przebieg_wymiany);
-        rodzaj = (TextView) findViewById(R.id.rodzaj_oleju);
-        dolewka = (TextView) findViewById(R.id.dolewka_oleju);
+        dataSprawdzenia = findViewById(R.id.data_sprawdzenia);
+        przebiegSprawdzenia = findViewById(R.id.przebieg_sprawdzenia);
+        dataWymiany = findViewById(R.id.data_wymiany);
+        przebiegWymiany = findViewById(R.id.przebieg_wymiany);
+        rodzaj = findViewById(R.id.rodzaj_oleju);
+        dolewka = findViewById(R.id.dolewka_oleju);
 
 
 //ustawianie dzisiejszej daty i przypisanie
@@ -138,6 +119,7 @@ public class Oil_layout extends AppCompatActivity {
         pobierz_przebieg();
         aktualizuj_spr();
         dolewka();
+        historias = null;
         historias = getSavedArrayList();
         historias.add("Data: " + oil.getData_sprawdzenia() + " Przebieg: " + oil.getPrzegieg_sprawdzenia());
 
@@ -166,8 +148,8 @@ public class Oil_layout extends AppCompatActivity {
         month_x = cal.get(Calendar.MONTH) + 1;
         day_x = cal.get(Calendar.DAY_OF_MONTH);
         String dateString = day_x + "-" + month_x + "-" + year_x;
-        TextView data_dzis = (TextView) findViewById(R.id.data_dzisiejsza_s);
-        TextView data_dzis2 = (TextView) findViewById(R.id.data_dzisiejsza_w);
+        TextView data_dzis = findViewById(R.id.data_dzisiejsza_s);
+        TextView data_dzis2 = findViewById(R.id.data_dzisiejsza_w);
         data_dzis.setText(dateString);
         data_dzis2.setText(dateString);
         oil.setData_sprawdzenia(dateString);
@@ -177,11 +159,10 @@ public class Oil_layout extends AppCompatActivity {
 
     public void aktualizuj_spr() {
 
-
-        data_sprawdzenia = (TextView) findViewById(R.id.data_sprawdzenia);
-        data_sprawdzenia.setText(oil.getData_sprawdzenia());
-        przebieg_sprawdzenia = (TextView) findViewById(R.id.przebieg_sprawdzenia);
-        przebieg_sprawdzenia.setText(oil.getPrzegieg_sprawdzenia());
+        dataSprawdzenia = findViewById(R.id.data_sprawdzenia);
+        dataSprawdzenia.setText(oil.getData_sprawdzenia());
+        przebiegSprawdzenia = findViewById(R.id.przebieg_sprawdzenia);
+        przebiegSprawdzenia.setText(oil.getPrzegieg_sprawdzenia());
 
 
     }
@@ -189,15 +170,15 @@ public class Oil_layout extends AppCompatActivity {
     public void pobierz_przebieg() {
         Double roznica;
 
-        EditText Przebieg_sprawdzenia = (EditText) findViewById(R.id.wprowadz_przebieg_sprawdzenia);
-        EditText Przebieg_wymiany = (EditText) findViewById(R.id.wprowadz_przebieg_wymiany);
+        EditText Przebieg_sprawdzenia = findViewById(R.id.wprowadz_przebieg_sprawdzenia);
+        EditText Przebieg_wymiany = findViewById(R.id.wprowadz_przebieg_wymiany);
 
 
         //przebieg set
         oil.setPrzegieg_sprawdzenia(Przebieg_sprawdzenia.getText().toString());
         oil.setPrzebieg_wymiany(Przebieg_wymiany.getText().toString());
 
-        EditText Rodzaj = (EditText) findViewById(R.id.wprowadz_rodzaj_oleju);
+        EditText Rodzaj = findViewById(R.id.wprowadz_rodzaj_oleju);
         oil.setRodzaj(Rodzaj.getText().toString());
     }
 
@@ -210,10 +191,10 @@ public class Oil_layout extends AppCompatActivity {
         String przebieg_wczytany_wym = sharedPreferences.getString("przebieg_wczy", "");
         String rodzaj_wczytany = sharedPreferences.getString("rodzaj_wczy", "");
         String dolewkaString = sharedPreferences.getString("dolewka", "");
-        data_sprawdzenia.setText(data_wczytana);
-        data_wymiany.setText(data_wczytana_wym);
-        przebieg_wymiany.setText(przebieg_wczytany_wym);
-        przebieg_sprawdzenia.setText(przebieg_wczytany);
+        dataSprawdzenia.setText(data_wczytana);
+        dataWymiany.setText(data_wczytana_wym);
+        przebiegWymiany.setText(przebieg_wczytany_wym);
+        przebiegSprawdzenia.setText(przebieg_wczytany);
         rodzaj.setText(rodzaj_wczytany);
         dolewka.setText(dolewkaString);
 
@@ -227,10 +208,10 @@ public class Oil_layout extends AppCompatActivity {
         super.onPause();
         SharedPreferences sharedPreferences = getSharedPreferences("MyData", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("data_spr", data_sprawdzenia.getText().toString());
-        editor.putString("przebieg_spr", przebieg_sprawdzenia.getText().toString());
-        editor.putString("data_wym", data_wymiany.getText().toString());
-        editor.putString("przebieg_wczy", przebieg_wymiany.getText().toString());
+        editor.putString("data_spr", dataSprawdzenia.getText().toString());
+        editor.putString("przebieg_spr", przebiegSprawdzenia.getText().toString());
+        editor.putString("data_wym", dataWymiany.getText().toString());
+        editor.putString("przebieg_wczy", przebiegWymiany.getText().toString());
         editor.putString("rodzaj_wczy", rodzaj.getText().toString());
         editor.putString("dolewka", dolewka.getText().toString());
         editor.commit();
@@ -241,17 +222,15 @@ public class Oil_layout extends AppCompatActivity {
     public void dodaj_wym(View view) {
 
         pobierz_przebieg();
-
-        data_wymiany = (TextView) findViewById(R.id.data_wymiany);
-        data_wymiany.setText(oil.getData_wymiany());
-        przebieg_wymiany = (TextView) findViewById(R.id.przebieg_wymiany);
-        przebieg_wymiany.setText(oil.getPrzebieg_wymiany() + " km");
-        rodzaj = (TextView) findViewById(R.id.rodzaj_oleju);
+        dataWymiany = findViewById(R.id.data_wymiany);
+        dataWymiany.setText(oil.getData_wymiany());
+        przebiegWymiany = findViewById(R.id.przebieg_wymiany);
+        przebiegWymiany.setText(oil.getPrzebieg_wymiany() + " km");
+        rodzaj = findViewById(R.id.rodzaj_oleju);
         rodzaj.setText("Rodzaj: " + oil.getRodzaj());
-        historias_wymiany = getSavedArrayList2();
-        historias_wymiany.add("Data: " + oil.getData_wymiany() + "\nPrzebieg: " + oil.getPrzebieg_wymiany() + " Rodzaj: " + oil.getRodzaj());
-
-        saveArrayList2(historias_wymiany);
+        historiasWymiany = getSavedArrayList2();
+        historiasWymiany.add("Data: " + oil.getData_wymiany() + "\nPrzebieg: " + oil.getPrzebieg_wymiany() + " Rodzaj: " + oil.getRodzaj());
+        saveArrayList2(historiasWymiany);
 
 
     }
@@ -326,7 +305,7 @@ public class Oil_layout extends AppCompatActivity {
         this.arraySpinner = new String[]{
                 "Brak", "25 ml", "50 ml", "75 ml", "100 ml", "125 ml", "150 ml", "175 ml", "200 ml", "250 ml", "300 ml", "350 ml", "400 ml", "500 ml", "600 ml", "700 ml", "800 ml", "900 ml", "1000 ml",
         };
-        s = (Spinner) findViewById(R.id.spinner_dolewka);
+        s = findViewById(R.id.spinner_dolewka);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_dropdown_item, arraySpinner);
         s.setAdapter(adapter);
@@ -334,9 +313,9 @@ public class Oil_layout extends AppCompatActivity {
     }
 
     public void wyswietlwymiane() {
-        historias_wymiany = getSavedArrayList2();
+        historiasWymiany = getSavedArrayList2();
         Intent intent2 = new Intent(Oil_layout.this, HistoriaWymian.class);
-        intent2.putExtra("przenies2", historias_wymiany);
+        intent2.putExtra("przenies2", historiasWymiany);
         startActivity(intent2);
     }
 }
